4c0f5ae503d385aafa4f35000629acf7f44abef3
Derek Brown <djb3718@rit.edu>
2014-11-20 21:07:22 -0500

    Added repo to eclipse and created java file

---
+++ CSCI261_hw6_p2/src/OneWay.java
@@ -0,0 +1,24 @@
+/**
+ * OneWay.java
+ * 
+ * @author	Derek Brown
+ *
+ * Purpose:	To determine if there exists a single edge that you can add to the
+ * 			graph that would make it strongly connected and if such an edge
+ * 			exists, display it.
+ */
+
+import java.util.Scanner;
+
+public class OneWay {
+
+	// Hidden data members
+	
+	// Constructor
+	
+	// Methods
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end OneWay class
